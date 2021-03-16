fc6ac3c3cbb4b57601d7f4651451453e54bb5312
Jose Pernia <josepernia91@gmail.com>
2021-03-16 09:08:49 -0400

    Exercise 01

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,12 +3,11 @@
 
 import * as React from 'react'
 
-function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+function Greeting({ nameProp = '' }) {
+  const [name, setName] = React.useState(nameProp)
 
   function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
   }
 
   return (
@@ -23,7 +22,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting nameProp="Kent" />
 }
 
 export default App
